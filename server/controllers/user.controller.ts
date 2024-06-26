c928c36ffea013ab615ee5f6465001ee9a6789ac
RXGabriel <gabriel2005rf@gmail.com>
2024-06-26 18:19:51 -0300

    feat: add logout functionality with user session cleanup

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -7,6 +7,7 @@ import jwt, { Secret } from "jsonwebtoken";
 import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
+import { redis } from "../utils/redis";
 
 require("dotenv").config();
 
@@ -156,6 +157,10 @@ export const logoutUser = CatchAsyncError(
     try {
       res.cookie("access_token", "", { maxAge: 1 });
       res.cookie("refresh_token", "", { maxAge: 1 });
+
+      const userId = req.user?._id || "";
+      redis.del(userId);
+
       res.status(200).json({
         success: true,
         message: "Logged out successfully",
