77b5a07ba5c63648125be2c7d560271676dbbe9d
RXGabriel <gabriel2005rf@gmail.com>
2024-06-26 17:42:22 -0300

    fix: ensure save process continues after password hashing

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -70,6 +70,7 @@ userSchema.pre<IUser>("save", async function (next) {
     next();
   }
   this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 userSchema.methods.SignAccessToken = function () {
