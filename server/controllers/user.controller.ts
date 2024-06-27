a62214c65bfd985cbbf711e04d1f6be80c0ab0e7
RXGabriel <gabriel2005rf@gmail.com>
2024-06-27 16:15:01 -0300

    feat: add updateUserInfo functionality

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -44,6 +44,11 @@ interface ISocialAuthBody {
   avatar: string;
 }
 
+interface IUpdateUserInfo {
+  name?: string;
+  email?: string;
+}
+
 export const registrationUser = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -256,6 +261,39 @@ export const socialAuth = CatchAsyncError(
       sendToken(user, 200, res);
     }
     try {
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+export const updateUserInfo = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name, email } = req.body as IUpdateUserInfo;
+      const userId = req.user?._id;
+      const user = await userModel.findById({ email });
+
+      if (email && user) {
+        const isEmailExist = await userModel.findOne({ email });
+
+        if (isEmailExist) {
+          return next(new ErrorHandler("Email already exist", 400));
+        }
+        user.email = email;
+      }
+
+      if (name && user) {
+        user.name = name;
+      }
+
+      await user?.save();
+      await redis.set(userId, JSON.stringify(user));
+
+      res.status(201).json({
+        success: true,
+        user,
+      });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
     }
