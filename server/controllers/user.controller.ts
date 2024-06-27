a0a00ef50e712bacfb9080a5f86a5c3d98b2d450
RXGabriel <gabriel2005rf@gmail.com>
2024-06-27 16:42:11 -0300

    fix: correct the userUpdateInfo logic

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -270,18 +270,10 @@ export const socialAuth = CatchAsyncError(
 export const updateUserInfo = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { name, email } = req.body as IUpdateUserInfo;
+      const { name } = req.body as IUpdateUserInfo;
+
       const userId = req.user?._id;
-      const user = await userModel.findById({ email });
-
-      if (email && user) {
-        const isEmailExist = await userModel.findOne({ email });
-
-        if (isEmailExist) {
-          return next(new ErrorHandler("Email already exist", 400));
-        }
-        user.email = email;
-      }
+      const user = await userModel.findById(userId);
 
       if (name && user) {
         user.name = name;
