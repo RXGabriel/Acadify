593ef887534f79d4a75cbb1fedc056398457ca65
RXGabriel <gabriel2005rf@gmail.com>
2024-06-27 16:15:13 -0300

    feat: add updateUserInfo route

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -7,6 +7,7 @@ import {
   registrationUser,
   socialAuth,
   updateAccessToken,
+  updateUserInfo,
 } from "../controllers/user.controller";
 import { isAuthenticated } from "../middleware/auth";
 
@@ -19,5 +20,6 @@ userRouter.get("/logout", isAuthenticated, logoutUser);
 userRouter.get("/refresh", updateAccessToken);
 userRouter.get("/me", isAuthenticated, getUserInfo);
 userRouter.post("/social-auth", socialAuth);
+userRouter.put("/update-user-info", isAuthenticated, updateUserInfo);
 
 export default userRouter;
