6649c55b6eb506faf7be389280dc43cf47344466
RXGabriel <gabriel2005rf@gmail.com>
2024-07-01 11:16:18 -0300

    feat: add course routes into main application

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./middleware/error";
 import userRouter from "./routes/user.route";
+import courseRouter from "./routes/course.route";
 
 require("dotenv").config();
 export const app = express();
@@ -10,7 +11,8 @@ export const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(cors({ origin: process.env.ORIGIN }));
-app.use("/api/v1", userRouter);
+
+app.use("/api/v1", userRouter, courseRouter);
 
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
