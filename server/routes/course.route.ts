b8e168ca121735a065e7a81619473d9adce35e67
RXGabriel <gabriel2005rf@gmail.com>
2024-07-02 15:27:56 -0300

    feat: implement addReview route

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -3,6 +3,7 @@ import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 import {
   addAnswer,
   addQuestion,
+  addReview,
   editCourse,
   getAllCourses,
   getCourseByUser,
@@ -31,5 +32,6 @@ courseRouter.get("/get-courses", getAllCourses);
 courseRouter.get("/get-course-content/:id", isAuthenticated, getCourseByUser);
 courseRouter.put("/add-question", isAuthenticated, addQuestion);
 courseRouter.put("/add-answer", isAuthenticated, addAnswer);
+courseRouter.put("/add-review/:id", isAuthenticated, addReview);
 
 export default courseRouter;
