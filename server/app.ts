28cc678a893f5a63cacc95dc5d47d45a53d3d8ab
RXGabriel <gabriel2005rf@gmail.com>
2024-07-02 17:45:57 -0300

    feat: add orderRouter to express app for handling order-related routes

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./middleware/error";
 import userRouter from "./routes/user.route";
 import courseRouter from "./routes/course.route";
+import orderRouter from "./routes/order.route";
 
 require("dotenv").config();
 export const app = express();
@@ -12,7 +13,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(cors({ origin: process.env.ORIGIN }));
 
-app.use("/api/v1", userRouter, courseRouter);
+app.use("/api/v1", userRouter, courseRouter, orderRouter);
 
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
