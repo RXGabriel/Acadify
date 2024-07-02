85cb1f292701baf91cae7f3a0cd2aede79b3548c
RXGabriel <gabriel2005rf@gmail.com>
2024-07-02 11:06:38 -0300

    feat: add addAnswer route

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 import {
+  addAnswer,
   addQuestion,
   editCourse,
   getAllCourses,
@@ -29,5 +30,6 @@ courseRouter.get("/get-course/:id", getSingleCourse);
 courseRouter.get("/get-courses", getAllCourses);
 courseRouter.get("/get-course-content/:id", isAuthenticated, getCourseByUser);
 courseRouter.put("/add-question", isAuthenticated, addQuestion);
+courseRouter.put("/add-answer", isAuthenticated, addAnswer);
 
 export default courseRouter;
