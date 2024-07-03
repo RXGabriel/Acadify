a195bfad3038791e9347179d873378b43d78a255
RXGabriel <gabriel2005rf@gmail.com>
2024-07-03 09:51:08 -0300

    feat: add notificationRouter to API routes

--- server/app.ts
+++ server/app.ts
@@ -5,6 +5,7 @@ import { ErrorMiddleware } from "./middleware/error";
 import userRouter from "./routes/user.route";
 import courseRouter from "./routes/course.route";
 import orderRouter from "./routes/order.route";
+import notificationRouter from "./routes/notification.route";
 
 require("dotenv").config();
 export const app = express();
@@ -13,7 +14,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(cors({ origin: process.env.ORIGIN }));
 
-app.use("/api/v1", userRouter, courseRouter, orderRouter);
+app.use("/api/v1", userRouter, courseRouter, orderRouter, notificationRouter);
 
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
