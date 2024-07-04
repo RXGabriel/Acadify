78213c942eadb1437756d2fbb483f7ee85991842
RXGabriel <gabriel2005rf@gmail.com>
2024-07-04 10:29:50 -0300

    feat: add getOrderAnalytics function

--- server/controllers/analytics.controller.ts
+++ server/controllers/analytics.controller.ts
@@ -4,6 +4,7 @@ import ErrorHandler from "../utils/ErrorHandler";
 import { generateLast12MonthsData } from "../utils/analytics.generator";
 import userModel from "../models/user.model";
 import CourseModel from "../models/course.model";
+import orderModel from "../models/orderModel";
 
 export const getUserAnalytics = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -28,6 +29,21 @@ export const getCoursesAnalytics = CatchAsyncError(
       res.status(200).json({
         success: true,
         courses,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+export const getOrderAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const orders = await generateLast12MonthsData(orderModel);
+
+      res.status(200).json({
+        success: true,
+        orders,
       });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
