35e4c800b8b9b21709b5439e79c557a07f01e120
RXGabriel <gabriel2005rf@gmail.com>
2024-07-04 10:28:30 -0300

    feat: add getCoursesAnalytics function

--- server/controllers/analytics.controller.ts
+++ server/controllers/analytics.controller.ts
@@ -3,6 +3,7 @@ import { CatchAsyncError } from "../middleware/catchAsyncError";
 import ErrorHandler from "../utils/ErrorHandler";
 import { generateLast12MonthsData } from "../utils/analytics.generator";
 import userModel from "../models/user.model";
+import CourseModel from "../models/course.model";
 
 export const getUserAnalytics = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -12,6 +13,21 @@ export const getUserAnalytics = CatchAsyncError(
       res.status(200).json({
         success: true,
         users,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+export const getCoursesAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const courses = await generateLast12MonthsData(CourseModel);
+
+      res.status(200).json({
+        success: true,
+        courses,
       });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
