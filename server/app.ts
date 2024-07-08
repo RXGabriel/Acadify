984ad33baa761591de0db1d4aa79f0e4f2b5116c
RXGabriel <gabriel2005rf@gmail.com>
2024-07-08 09:27:21 -0300

    feat: add layoutRouter to API routes

--- server/app.ts
+++ server/app.ts
@@ -7,6 +7,7 @@ import courseRouter from "./routes/course.route";
 import orderRouter from "./routes/order.route";
 import notificationRouter from "./routes/notification.route";
 import analyticsRouter from "./routes/analytics.route";
+import layoutRouter from "./routes/layout.route";
 
 require("dotenv").config();
 export const app = express();
@@ -21,7 +22,8 @@ app.use(
   courseRouter,
   orderRouter,
   notificationRouter,
-  analyticsRouter
+  analyticsRouter,
+  layoutRouter
 );
 
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
