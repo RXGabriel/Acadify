87c5b7f7cc7d6ae7543a68820077a05a0f4ff400
RXGabriel <gabriel2005rf@gmail.com>
2024-07-08 16:24:49 -0300

    feat: add access token update check

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -3,6 +3,7 @@ import { CatchAsyncError } from "./catchAsyncError";
 import ErrorHandler from "../utils/ErrorHandler";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { redis } from "../utils/redis";
+import { updateAccessToken } from "../controllers/user.controller";
 
 export const isAuthenticated = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -13,23 +14,30 @@ export const isAuthenticated = CatchAsyncError(
       );
     }
 
-    const decoded = jwt.verify(
-      access_token,
-      process.env.ACCESS_TOKEN as string
-    ) as JwtPayload;
+    const decoded = jwt.decode(access_token) as JwtPayload;
     if (!decoded) {
       return next(new ErrorHandler("access token is not valid", 400));
     }
 
-    const user = await redis.get(decoded.id);
-    if (!user) {
-      return next(
-        new ErrorHandler("Please login to access this resource", 400)
-      );
-    }
+    if (decoded.exp && decoded.exp <= Date.now() / 1000) {
+      try {
+        await updateAccessToken(req, res, next);
+      } catch (error) {
+        return next(error);
+      }
+    } else {
+      const user = await redis.get(decoded.id);
 
-    req.user = JSON.parse(user);
-    next();
+      if (!user) {
+        return next(
+          new ErrorHandler("Please login to access this resource", 400)
+        );
+      }
+
+      req.user = JSON.parse(user);
+
+      next();
+    }
   }
 );
 
