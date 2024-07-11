ccecc4cfd4998ea8e2fad3e3d0bfa2d9f61d8684
RXGabriel <gabriel2005rf@gmail.com>
2024-07-11 15:29:08 -0300

    feat: integrate CustomModal and Login component into Header with state management for routing

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -4,14 +4,18 @@ import React, { FC, useState } from "react";
 import { ThemeSwitcher } from "../utils/ThemeSwitcher";
 import { HiOutlineMenuAlt3, HiOutlineUserCircle } from "react-icons/hi";
 import NavItems from "../utils/NavItems";
+import CustomModel from "../utils/CustomModal";
+import Login from "../components/Auth/Login";
 
 type Props = {
   open: boolean;
   setOpen: (open: boolean) => void;
   activeItem: number;
+  route: string;
+  setRoute: (route: string) => void;
 };
 
-const Header: FC<Props> = ({ activeItem, setOpen }) => {
+const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
   const [active, setActive] = useState(false);
   const [openSidebar, setOpenSidebar] = useState(false);
 
@@ -94,6 +98,19 @@ const Header: FC<Props> = ({ activeItem, setOpen }) => {
           </div>
         )}
       </div>
+      {route === "Login" && (
+        <>
+          {open && (
+            <CustomModel
+              open={open}
+              setOpen={setOpen}
+              setRoute={setRoute}
+              activeItem={activeItem}
+              component={Login}
+            />
+          )}
+        </>
+      )}
     </div>
   );
 };
