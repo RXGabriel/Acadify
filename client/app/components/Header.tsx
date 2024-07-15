ede51dac2e5008766a5892116093148560bad5b1
RXGabriel <gabriel2005rf@gmail.com>
2024-07-15 10:10:07 -0300

    feat: add Verification component to Header for account verification

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -7,6 +7,7 @@ import NavItems from "../utils/NavItems";
 import CustomModal from "../utils/CustomModal";
 import Login from "../components/Auth/Login";
 import SignUp from "../components/Auth/SignUp";
+import Verification from "../components/Auth/Verification";
 
 type Props = {
   open: boolean;
@@ -122,6 +123,20 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
               setRoute={setRoute}
               activeItem={activeItem}
               component={SignUp}
+            />
+          )}
+        </>
+      )}
+
+      {route === "Verification" && (
+        <>
+          {open && (
+            <CustomModal
+              open={open}
+              setOpen={setOpen}
+              setRoute={setRoute}
+              activeItem={activeItem}
+              component={Verification}
             />
           )}
         </>
