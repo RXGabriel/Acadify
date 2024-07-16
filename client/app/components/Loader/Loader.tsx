1b9d951bb47b96e0185444d90324aab632e30ea4
RXGabriel <gabriel2005rf@gmail.com>
2024-07-16 16:39:12 -0300

    refactor: convert Loader component to arrow function syntax

--- client/app/components/Loader/Loader.tsx
+++ client/app/components/Loader/Loader.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Loader.css";
 
-function Loader() {
+const Loader = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="loader"></div>
     </div>
   );
-}
+};
 
 export default Loader;
