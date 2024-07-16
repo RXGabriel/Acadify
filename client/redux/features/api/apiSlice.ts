ef1eeefa7256d31410b78d9bcbc711c8f011890a
RXGabriel <gabriel2005rf@gmail.com>
2024-07-16 16:14:56 -0300

    refactor: rename loadUSer to loadUser in apiSlice

--- client/redux/features/api/apiSlice.ts
+++ client/redux/features/api/apiSlice.ts
@@ -14,7 +14,7 @@ export const apiSlice = createApi({
         credentials: "include" as const,
       }),
     }),
-    loadUSer: builder.query({
+    loadUser: builder.query({
       query: (data) => ({
         url: "me",
         method: "GET",
@@ -25,7 +25,7 @@ export const apiSlice = createApi({
           const result = await queryFulfilled;
           dispatch(
             userLoggedIn({
-              accessToken: result.data.activationToken,
+              accessToken: result.data.accessToken,
               user: result.data.user,
             })
           );
@@ -37,4 +37,4 @@ export const apiSlice = createApi({
   }),
 });
 
-export const { useRefreshTokenQuery, useLoadUSerQuery } = apiSlice;
+export const { useRefreshTokenQuery, useLoadUserQuery } = apiSlice;
