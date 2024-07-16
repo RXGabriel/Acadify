e58b06775c9d8424afef73351c1c5af9547199ef
RXGabriel <gabriel2005rf@gmail.com>
2024-07-16 09:48:38 -0300

    feat: add user avatar display and profile link in Header component

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -8,6 +8,9 @@ import CustomModal from "../utils/CustomModal";
 import Login from "../components/Auth/Login";
 import SignUp from "../components/Auth/SignUp";
 import Verification from "../components/Auth/Verification";
+import { useSelector } from "react-redux";
+import Image from "next/image";
+import avatar from "../../public/assets/avatar.png";
 
 type Props = {
   open: boolean;
@@ -20,6 +23,7 @@ type Props = {
 const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
   const [active, setActive] = useState(false);
   const [openSidebar, setOpenSidebar] = useState(false);
+  const { user } = useSelector((state: any) => state.auth);
 
   if (typeof window !== "undefined") {
     window.addEventListener("scroll", () => {
@@ -72,11 +76,27 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
                     />
                   </div>
 
-                  <HiOutlineUserCircle
-                    size={25}
-                    className="hidden 800px:block cursor-pointer dark:text-white text-black"
-                    onClick={() => setOpen(true)}
-                  />
+                  {user ? (
+                    <Link href={"/profile"}>
+                      <Image
+                        src={user.avatar ? user.avatar : avatar}
+                        alt=""
+                        width={30}
+                        height={30}
+                        className="w-[30px] h-[30px] rounded-full cursor-pointer"
+                        style={{
+                          border:
+                            activeItem === 5 ? "2px solid #37a39a" : "none",
+                        }}
+                      />
+                    </Link>
+                  ) : (
+                    <HiOutlineUserCircle
+                      size={25}
+                      className="hidden 800px:block cursor-pointer dark:text-white text-black"
+                      onClick={() => setOpen(true)}
+                    />
+                  )}
                 </div>
               </div>
             </div>
