f9da78fb51b32df8d4a0463b8cd988c162e4c8a2
RXGabriel <gabriel2005rf@gmail.com>
2024-07-22 12:18:30 -0300

    fix: update icon colors and dark mode styles

--- client/app/components/Auth/SignUp.tsx
+++ client/app/components/Auth/SignUp.tsx
@@ -114,13 +114,15 @@ const SignUp: FC<Props> = ({ setRoute }) => {
           />
           {!show ? (
             <AiOutlineEyeInvisible
-              className="absolute bottom-3 right-2 z-1 cursor-pointer"
+              color="black"
+              className="absolute bottom-3 right-2 z-1 cursor-pointer dark:bg-slate-600 "
               size={20}
               onClick={() => setShow(true)}
             />
           ) : (
             <AiOutlineEye
-              className="absolute bottom-3 right-2 z-1 cursor-pointer"
+              color="black"
+              className="absolute bottom-3 right-2 z-1 cursor-pointer dark:bg-slate-600 "
               size={20}
               onClick={() => setShow(false)}
             />
@@ -138,9 +140,13 @@ const SignUp: FC<Props> = ({ setRoute }) => {
         </h5>
         <div className="flex items-center justify-center my-3">
           <FcGoogle size={30} className="cursor-pointer mr-2" />
-          <AiFillGithub size={30} className="cursor-pointer ml-2" />
+          <AiFillGithub
+            color="black"
+            size={30}
+            className="cursor-pointer ml-2 dark:bg-white"
+          />
         </div>
-        <h5 className="text-center pt-4 font-Poppins text-[14px]">
+        <h5 className="text-center pt-4 font-Poppins text-[14px] text-black dark:text-white">
           Already have an account?{" "}
           <span
             className="text-[#2190ff] pl-1 cursor-pointer"
