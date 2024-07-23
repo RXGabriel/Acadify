b3c9499adfc132435ca703c5678a0f283a23bc07
RXGabriel <gabriel2005rf@gmail.com>
2024-07-23 10:42:52 -0300

    feat: add loader and session-based user data fetching to Header component

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -8,7 +8,6 @@ import CustomModal from "../utils/CustomModal";
 import Login from "../components/Auth/Login";
 import SignUp from "../components/Auth/SignUp";
 import Verification from "../components/Auth/Verification";
-import { useSelector } from "react-redux";
 import Image from "next/image";
 import avatar from "../../public/assets/avatar.png";
 import { useSession } from "next-auth/react";
@@ -16,7 +15,9 @@ import {
   useLogOutQuery,
   useSocialAuthMutation,
 } from "@/redux/features/auth/authApi";
-import toast from "react-hot-toast";
+import { toast } from "react-hot-toast";
+import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
+import Loader from "./Loader/Loader";
 
 type Props = {
   open: boolean;
@@ -29,7 +30,7 @@ type Props = {
 const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
   const [active, setActive] = useState(false);
   const [openSidebar, setOpenSidebar] = useState(false);
-  const { user } = useSelector((state: any) => state.auth);
+  const { data: userData, isLoading } = useLoadUserQuery(undefined, {});
   const { data } = useSession();
   const [socialAuth, { isSuccess, error }] = useSocialAuthMutation();
   const [logout, setLogout] = useState(false);
@@ -39,24 +40,26 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
   });
 
   useEffect(() => {
-    if (!user) {
-      if (data) {
-        socialAuth({
-          email: data?.user?.email,
-          name: data?.user?.name,
-          avatar: data.user?.image,
-        });
+    if (!isLoading) {
+      if (!userData) {
+        if (data) {
+          socialAuth({
+            email: data?.user?.email,
+            name: data?.user?.name,
+            avatar: data.user?.image,
+          });
+        }
+      }
+      if (data === null) {
+        if (isSuccess) {
+          toast.success("Login Successfully");
+        }
+      }
+      if (data === null && !isLoading && !userData) {
+        setLogout(true);
       }
     }
-    if (data === null) {
-      if (isSuccess) {
-        toast.success("Login Successfully");
-      }
-    }
-    if (data === null) {
-      setLogout(true);
-    }
-  }, [data, user]);
+  }, [data, userData, isLoading]);
 
   if (typeof window !== "undefined") {
     window.addEventListener("scroll", () => {
@@ -78,7 +81,9 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
 
   return (
     <>
-      {
+      {isLoading ? (
+        <Loader />
+      ) : (
         <div className="w-full relative">
           <div
             className={`${
@@ -108,11 +113,14 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
                       onClick={() => setOpenSidebar(true)}
                     />
                   </div>
-
-                  {user ? (
+                  {userData ? (
                     <Link href={"/profile"}>
                       <Image
-                        src={user.avatar ? user.avatar : avatar}
+                        src={
+                          userData?.user.avatar
+                            ? userData.user.avatar.url
+                            : avatar
+                        }
                         alt=""
                         width={30}
                         height={30}
@@ -143,13 +151,31 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
               >
                 <div className="w-[70%] fixed z-[999999999] h-screen bg-white dark:bg-slate-900 dark:bg-opacity-90 top-0 right-0">
                   <NavItems activeItem={activeItem} isMobile={true} />
-
-                  <HiOutlineUserCircle
-                    size={25}
-                    className="hidden 800px:block cursor-pointer dark:text-white text-black"
-                    onClick={() => setOpen(true)}
-                  />
-
+                  {userData?.user ? (
+                    <Link href={"/profile"}>
+                      <Image
+                        src={
+                          userData?.user.avatar
+                            ? userData.user.avatar.url
+                            : avatar
+                        }
+                        alt=""
+                        width={30}
+                        height={30}
+                        className="w-[30px] h-[30px] rounded-full ml-[20px] cursor-pointer"
+                        style={{
+                          border:
+                            activeItem === 5 ? "2px solid #37a39a" : "none",
+                        }}
+                      />
+                    </Link>
+                  ) : (
+                    <HiOutlineUserCircle
+                      size={25}
+                      className="hidden 800px:block cursor-pointer dark:text-white text-black"
+                      onClick={() => setOpen(true)}
+                    />
+                  )}
                   <br />
                   <br />
                   <p className="text-[16px] px-2 pl-5 text-black dark:text-white">
@@ -201,7 +227,7 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
             </>
           )}
         </div>
-      }
+      )}
     </>
   );
 };
