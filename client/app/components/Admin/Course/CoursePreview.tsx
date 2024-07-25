202395a670ac198bcb1eaf56c66d1a49f5562fc7
RXGabriel <gabriel2005rf@gmail.com>
2024-07-25 17:06:39 -0300

    feat: add CoursePlayer and styling to CoursePreview component

--- client/app/components/Admin/Course/CoursePreview.tsx
+++ client/app/components/Admin/Course/CoursePreview.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from "react";
+import CoursePlayer from "../../../utils/CoursePlayer";
+import { styles } from "../../../../app/styles/style";
 
 type Props = {
   active: number;
@@ -33,7 +35,69 @@ const CoursePreview: FC<Props> = ({
   return (
     <div className="w-[90%] m-auto py-5 mb-5">
       <div className="w-full relative">
-        <div className="w-full mt-10 dark:text-white text-black"></div>
+        <div className="w-full mt-10 dark:text-white text-black">
+          <CoursePlayer
+            videoUrl={courseData?.demoUrl}
+            title={courseData?.title}
+          />
+        </div>
+        <div className="flex items-center">
+          <h1 className="pt-5 text-[25px] dark:text-white text-black">
+            {courseData?.price === 0 ? "Free" : courseData?.price + "$"}
+          </h1>
+          <h5 className="pl-3 text-[20px] mt-2 line-through opacity-80 dark:text-white text-black">
+            {courseData?.estimatedPrice}$
+          </h5>
+
+          <h4 className="pl-5 pt-4 text-[22px] dark:text-white text-black">
+            {discountPercentagePrice}% Off
+          </h4>
+        </div>
+
+        <div className="flex items-center">
+          <div
+            className={`${styles.button} !w-[180px] my-3 font-Poppins !bg-[crimson] cursor-not-allowed`}
+          >
+            Buy Now {courseData?.price}$
+          </div>
+        </div>
+
+        <div className="flex items-center">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Discount code..."
+            className={`${styles.input} 1500px:!w-[50%] 1100px:w-[60%] ml-3 !mt-0`}
+          />
+          <div
+            className={`${styles.button} !w-[120px] my-3 ml-4 font-Poppins cursor-pointer`}
+          >
+            Apply
+          </div>
+        </div>
+        <p className="pb-1 dark:text-white text-black">
+          • Source code included
+        </p>
+        <p className="pb-1 dark:text-white text-black">
+          • Full lifetime access
+        </p>
+        <p className="pb-1 dark:text-white text-black">
+          • Certificate of completion
+        </p>
+        <p className="pb-3 800px:pb-1 dark:text-white text-black">
+          • Premium Support
+        </p>
+      </div>
+      <div className="w-full">
+        <div className="w-full 800px:pr-5">
+          <h1 className="text-[25px] font-Poppins font-[600] dark:text-white text-black">
+            {courseData?.name}
+          </h1>
+          <div className="flex items-center justify-between pt-3">
+            {/* Additional content goes here */}
+          </div>
+        </div>
       </div>
     </div>
   );
