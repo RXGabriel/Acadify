f792e5955a5bbbe06c3127fd5ce90d0d1ab44336
RXGabriel <gabriel2005rf@gmail.com>
2024-07-26 17:13:05 -0300

    feat: add team admin page

---
+++ client/app/admin/team/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React from "react";
+import DashboardHero from "@/app/components/Admin/DashboardHero";
+import AdminProtected from "@/app/hooks/adminProtected";
+import Heading from "@/app/utils/Heading";
+import AdminSidebar from "../../components/Admin/sidebar/AdminSidebar";
+import AllCourses from "@/app/components/Admin/Course/AllCourses";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <AdminProtected>
+        <Heading
+          title="Acadify - Admin"
+          description="Acadify is a platform from students to learn and get help from teachers"
+          keywords="Programming, MERN, Courses, Machine Learning"
+        />
+        <div className="flex h-screen">
+          <div className="1500px:w-[16%] w-1/5">
+            <AdminSidebar />
+          </div>
+          <div className="w-[85%]">
+            <DashboardHero />
+            <AllCourses />
+          </div>
+        </div>
+      </AdminProtected>
+    </div>
+  );
+};
+
+export default page;
