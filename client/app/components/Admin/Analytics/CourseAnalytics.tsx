b329d510bdcb8b89db122ff32efe679a05a6b671
RXGabriel <gabriel2005rf@gmail.com>
2024-07-30 15:51:04 -0300

    feat: add CourseAnalytics component

---
+++ client/app/components/Admin/Analytics/CourseAnalytics.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Loader from "../../Loader/Loader";
+import { useGetCoursesAnalyticsQuery } from "@/redux/features/analytics/analyticsApi";
+import { styles } from "@/app/styles/style";
+import {
+  BarChart,
+  Bar,
+  ResponsiveContainer,
+  XAxis,
+  Label,
+  YAxis,
+  LabelList,
+} from "recharts";
+
+type Props = {};
+
+const CourseAnalytics = (props: Props) => {
+  const { data, isLoading } = useGetCoursesAnalyticsQuery({});
+  const analyticsData: any = [];
+  const minValue = 0;
+
+  data &&
+    data.course.last12Months.forEach((item: any) => {
+      analyticsData.push({ name: item.month, uv: item.count });
+    });
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="h-screen">
+          <div className="mt-[50px]">
+            <h1 className={`${styles.title} px-5 !text-start`}>
+              Course Analysis
+            </h1>
+            <p className={`${styles.label} px-5`}>
+              Analytics data from the last 12 months
+            </p>
+          </div>
+
+          <div className="w-full h-[90%] flex items-center justify-center">
+            <ResponsiveContainer width="90%" height="50%">
+              <BarChart width={150} height={300} data={analyticsData}>
+                <XAxis dataKey="name">
+                  <Label offset={0} position="insideBottom" />
+                </XAxis>
+                <YAxis domain={[minValue, "auto"]} />
+                <Bar dataKey="uv" fill="#3faf82">
+                  <LabelList dataKey="uv" position="top" />
+                </Bar>
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CourseAnalytics;
