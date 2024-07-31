ef30c6d00305726309eb45669d166be4cde33ae9
RXGabriel <gabriel2005rf@gmail.com>
2024-07-31 17:00:31 -0300

    fix: remove unnecessary next parameter

--- server/services/order.service.ts
+++ server/services/order.service.ts
@@ -2,16 +2,14 @@ import { NextFunction, Response } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncError";
 import OrderModel from "../models/order.model";
 
-export const newOrder = CatchAsyncError(
-  async (data: any, next: NextFunction, res: Response) => {
-    const order = await OrderModel.create(data);
+export const newOrder = CatchAsyncError(async (data: any, res: Response) => {
+  const order = await OrderModel.create(data);
 
-    res.status(201).json({
-      success: true,
-      order,
-    });
-  }
-);
+  res.status(201).json({
+    success: true,
+    order,
+  });
+});
 
 export const getAllOrdersService = async (res: Response) => {
   const orders = await OrderModel.find().sort({ createdAt: -1 });
