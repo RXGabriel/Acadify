87b32417b434e00430dbd48cc0990c637da7b72a
RXGabriel <gabriel2005rf@gmail.com>
2024-08-02 16:35:58 -0300

    feat: add CourseDetails Page

---
+++ client/app/components/Course/CourseDetailsPage.tsx
@@ -0,0 +1,50 @@
+import { useGetCourseDetailsQuery } from "@/redux/features/courses/coursesApi";
+import React, { useState } from "react";
+import Loader from "../Loader/Loader";
+import Heading from "@/app/utils/Heading";
+import Header from "../Header";
+import CourseDetails from "./CourseDetails";
+import Footer from "../Footer";
+
+type Props = {
+  id: string;
+};
+
+const CourseDetailsPage = ({ id }: Props) => {
+  const [route, setRoute] = useState("Login");
+  const [open, setOpen] = useState(false);
+  const { data, isLoading } = useGetCourseDetailsQuery(id);
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Heading
+            title={data?.course?.name + " - Acadify "}
+            description={
+              "Acadify is a programming community which is developed by RXGabriel for helping programmers"
+            }
+            keywords={data?.course?.tags}
+          />
+          <Header
+            route={route}
+            setRoute={setRoute}
+            open={open}
+            setOpen={setOpen}
+            activeItem={1}
+          />
+          <CourseDetails
+            data={data.course}
+            setRoute={setRoute}
+            setOpen={setOpen}
+          />
+          <Footer />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CourseDetailsPage;
