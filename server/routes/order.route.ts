9a9258ae8a97de9af7a03783d39ccb55189a2d89
RXGabriel <gabriel2005rf@gmail.com>
2024-08-05 10:16:07 -0300

    feat: add sendStripePublishableKey route

--- server/routes/order.route.ts
+++ server/routes/order.route.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { createOrder, getAllOrders } from "../controllers/order.controller";
+import {
+  createOrder,
+  getAllOrders,
+  sendStripePublishableKey,
+} from "../controllers/order.controller";
 
 const orderRouter = express.Router();
 
@@ -11,5 +15,6 @@ orderRouter.get(
   authorizeRoles("admin"),
   getAllOrders
 );
+orderRouter.get("/payment/stripepublishablekey", sendStripePublishableKey);
 
 export default orderRouter;
