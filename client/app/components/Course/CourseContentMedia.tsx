7325a200aad42b0d1d2af813f09a67d176a2507f
RXGabriel <gabriel2005rf@gmail.com>
2024-08-06 11:57:30 -0300

    feat: add navigation in CourseContentMedia

--- client/app/components/Course/CourseContentMedia.tsx
+++ client/app/components/Course/CourseContentMedia.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { styles } from "@/app/styles/style";
 import CoursePlayer from "@/app/utils/CoursePlayer";
-import { useGetCourseDetailsQuery } from "@/redux/features/courses/coursesApi";
+import {
+  useAddNewQuestionMutation,
+  useGetCourseDetailsQuery,
+} from "@/redux/features/courses/coursesApi";
 import Image from "next/image";
 import { format } from "timeago.js";
 import { toast } from "react-hot-toast";
@@ -23,8 +26,188 @@ type Props = {
   user: any;
 };
 
-const CourseContentMedia = (props: Props) => {
-  return <div>CourseContentMedia</div>;
+const CourseContentMedia = ({
+  data,
+  id,
+  activeVideo,
+  setActiveVideo,
+  user,
+}: Props) => {
+  const [activeBar, setActiveBar] = useState(0);
+  const [comment, setComment] = useState("");
+  const [question, setQuestion] = useState("");
+  const { data: courseData, refetch: courseRefetch } = useGetCourseDetailsQuery(
+    id,
+    { refetchOnMountOrArgChange: true }
+  );
+  const course = courseData?.course;
+
+  const [
+    addNewQuestion,
+    { isSuccess, error, isLoading: questionCreationLoading },
+  ] = useAddNewQuestionMutation();
+
+  const handleQuestion = () => {
+    if (question.length === 0) {
+      toast.error("Question can't be empty");
+    } else {
+      addNewQuestion({
+        question,
+        courseId: id,
+        contentId: data[activeVideo]?._id,
+      });
+    }
+  };
+
+  const isReviewExists = course?.reviews?.find(
+    (item: any) => item.user._id === user._id
+  );
+
+  return (
+    <div className="w-[95%] 800px:w-[86%] py-4 m-auto">
+      <CoursePlayer
+        title={data[activeVideo]?.title}
+        videoUrl={data[activeVideo]?.videoUrl}
+      />
+
+      <div className="w-full flex items-center justify-between my-3">
+        <div
+          className={`${
+            styles.button
+          } text-white  !w-[unset] !min-h-[40px] !py-[unset] ${
+            activeVideo === 0 && "!cursor-no-drop opacity-[.8]"
+          }`}
+          onClick={() =>
+            setActiveVideo(activeVideo === 0 ? 0 : activeVideo - 1)
+          }
+        >
+          <AiOutlineArrowLeft className="mr-2" />
+          Prev Lesson
+        </div>
+
+        <div
+          className={`${
+            styles.button
+          } !w-[unset] text-white  !min-h-[40px] !py-[unset] ${
+            data.length - 1 === activeVideo && "!cursor-no-drop opacity-[.8]"
+          }`}
+          onClick={() =>
+            setActiveVideo(
+              data && data.length - 1 === activeVideo
+                ? activeVideo
+                : activeVideo + 1
+            )
+          }
+        >
+          Next Lesson
+          <AiOutlineArrowRight className="ml-2" />
+        </div>
+      </div>
+
+      <h1 className="pt-2 text-[25px] font-[600] dark:text-white text-black">
+        {data[activeVideo].title}
+      </h1>
+      <br />
+
+      <div className="w-full p-4 flex items-center justify-between bg-slate-500 bg-opacity-20 backdrop-blur shadow-[bg-slate-700] rounded shadow-inner">
+        {["Overview", "Resources", "Q&A", "Reviews"].map((text, index) => (
+          <h5
+            key={index}
+            className={`800px:text-[20px] cursor-pointer ${
+              activeBar === index
+                ? "text-red-500"
+                : "dark:text-white text-black"
+            }`}
+            onClick={() => setActiveBar(index)}
+          >
+            {text}
+          </h5>
+        ))}
+      </div>
+      <br />
+
+      {activeBar === 0 && (
+        <p className="text-[18px] whitespace-pre-line mb-3 dark:text-white text-black">
+          {data[activeVideo]?.description}
+        </p>
+      )}
+
+      {activeBar === 1 && (
+        <div>
+          {data[activeVideo]?.links.map((item: any, index: number) => (
+            <div className="mb-5" key={index}>
+              <h2 className="800px:text-[20px] 800px:inline-block dark:text-white text-black">
+                {item.title && item.tile + " :"}
+              </h2>
+              <a
+                className="inline-block text-[#4395c4] 800px:text-[20px] 800px:pl-2"
+                href={item.url}
+              >
+                {item.url}
+              </a>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {activeBar === 2 && (
+        <>
+          <div className="flex w-full">
+            <Image
+              src={
+                user.avatar
+                  ? user.avatar.url
+                  : "https://res.cloudinary.com/dshp9jnuy/image/upload/v1665822253/avatars/nrxsg8sd9iy10bbsoenn.png"
+              }
+              width={50}
+              height={50}
+              alt="Avatar Image"
+              className="w-[50px] h-[50px] rounded-full object-cover"
+            />
+            <textarea
+              name=""
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+              id=""
+              cols={40}
+              rows={5}
+              placeholder="Write your question here..."
+              className="outline-none bg-transparent ml-3 border dark:text-white text-black border-[#0000001d] dark:border-[#ffffff57] 800px:w-full p-2 rounded w-[90%] 800px:text-[18px] font-Poppins"
+            ></textarea>
+          </div>
+
+          <div className="w-full flex justify-end">
+            <div>
+              <div
+                className={`${
+                  styles.button
+                } !w-[120px] !h-[40px] text-[18px] mt-5 ${
+                  questionCreationLoading && "cursor-not-allowed"
+                }`}
+                onClick={questionCreationLoading ? () => {} : handleQuestion}
+              >
+                Submit
+              </div>
+            </div>
+            <br />
+            <br />
+
+            <div className="w-full h-[1px] bg-[#ffffff3b]">
+              <div></div>
+            </div>
+          </div>
+        </>
+      )}
+
+      {activeBar === 3 && (
+        <div className="w-full">
+          <>
+            <div></div>
+          </>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default CourseContentMedia;
