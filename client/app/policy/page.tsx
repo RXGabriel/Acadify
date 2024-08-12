e5d499eeb1e5d0587c61664ab2300e8372edec91
RXGabriel <gabriel2005rf@gmail.com>
2024-08-12 09:53:06 -0300

    feat: add policy page component

---
+++ client/app/policy/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React, { useState } from "react";
+import Heading from "../utils/Heading";
+import Header from "../components/Header";
+import Policy from "./Policy";
+import Footer from "../components/Footer";
+
+type Props = {};
+
+const Page = (props: Props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(2);
+  const [route, setRoute] = useState("Login");
+
+  return (
+    <div>
+      <Heading
+        title="Policy - Acadify"
+        description="Acadify is a platform for selling courses, providing a wide range of educational content to enhance your skills and knowledge."
+        keywords="Acadify, online courses, course sales, e-learning, education, skill development, learning platform, buy courses"
+      />
+      <Header
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
+        setRoute={setRoute}
+        route={route}
+      />
+      <Policy />
+      <Footer />
+    </div>
+  );
+};
+
+export default Page;
