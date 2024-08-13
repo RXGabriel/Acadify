27ee1557cc784229cd840daa744eda41346db636
RXGabriel <gabriel2005rf@gmail.com>
2024-08-13 15:14:23 -0300

    fix: add div wrapper

--- client/app/admin/categories/page.tsx
+++ client/app/admin/categories/page.tsx
@@ -11,22 +11,24 @@ type Props = {};
 
 const page = (props: Props) => {
   return (
-    <AdminProtected>
-      <Heading
-        title="Acadify - Admin"
-        description="Acadify is a platform from students to learn and get help from teachers"
-        keywords="Programming, MERN, Courses, Machine Learning"
-      />
-      <div className="flex h-screen">
-        <div className="1500px:w-[16%] w-1/5">
-          <AdminSideBar />
+    <div>
+      <AdminProtected>
+        <Heading
+          title="Acadify - Admin"
+          description="Acadify is a platform from students to learn and get help from teachers"
+          keywords="Programming, MERN, Courses, Machine Learning"
+        />
+        <div className="flex h-screen">
+          <div className="1500px:w-[16%] w-1/5">
+            <AdminSideBar />
+          </div>
+          <div className="w-[85%]">
+            <DashboardHero />
+            <EditCategories />
+          </div>
         </div>
-        <div className="w-[85%]">
-          <DashboardHero />
-          <EditCategories />
-        </div>
-      </div>
-    </AdminProtected>
+      </AdminProtected>
+    </div>
   );
 };
 
