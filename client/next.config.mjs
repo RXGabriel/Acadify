f9c6bd49bd793e613316dd4397eec6489d63ee44
RXGabriel <gabriel2005rf@gmail.com>
2024-08-14 09:46:51 -0300

    chore: update config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ["res.cloudinary.com", "randomuser.me"] },
+  images: {
+    domains: ["res.cloudinary.com", "randomuser.me"],
+  },
   experimental: {
-    reactRoot: true,
-    suppressHydrationWarning: true,
+    // Remova ou ajuste essas opções conforme necessário
+    // reactRoot: true,
+    // suppressHydrationWarning: true,
   },
 };
 
